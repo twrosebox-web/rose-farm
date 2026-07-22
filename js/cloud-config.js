a9a1822c6f33dbaab4296ee8bd5b65e1c8624d46
twrosebox-web <twrosebox@gmail.com>
2026-07-22 12:13:14 +0800

    feat: 接上內容後台 Web App endpoint

--- js/cloud-config.js
+++ js/cloud-config.js
@@ -1,3 +1,3 @@
 window.CLOUD_CONFIG = Object.assign({
-    endpoint: ''
+    endpoint: 'https://script.google.com/macros/s/AKfycbzfvkX-bA57mr-vHJE0EBJVY9inw4DRNnpkcgbDb54x7-eurju6FfLtUTHvO_jEbigN/exec'
 }, window.CLOUD_CONFIG || {});
